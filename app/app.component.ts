5d8761225ba551e193169306e76f73f62b3531d0
Moulin Philippe <philippe.moulin@gfi.fr>
2018-08-29 23:08:36 +0200

    First component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  styleUrls:['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      {{ title }}
     </div>
   `
 })
 export class AppComponent {
-
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
